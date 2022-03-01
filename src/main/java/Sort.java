15eeaa645139cabd9010599a85199c823ec92e79
yourass <yu.v.mur@gmail.com>
2022-03-01 22:27:25 +0300

    Add selection sort

--- src/main/java/Sort.java
+++ src/main/java/Sort.java
@@ -1,11 +1,13 @@
-
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.util.Collections.swap;
 
 public class Sort {
 
-    public static int[] bubble (int[] array) {
+    // Bubble sort
+    public static int[] bubble(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
             boolean swapped = false;
             for (int j = 0; j < array.length - 1 - i; j++) {
@@ -22,7 +24,7 @@ public class Sort {
         return array;
     }
 
-    public static <T extends Comparable<T>> List<T> bubble(List<T> array) {
+    public static <T extends Comparable<? super T>> List<T> bubble(List<T> array) {
         for (int i = 0; i < array.size() - 1; i++) {
             boolean swapped = false;
             for (int j = 0; j < array.size() - 1 - i; j++) {
@@ -35,4 +37,36 @@ public class Sort {
         }
         return array;
     }
+
+    // Selection sort
+    public static int[] selection(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int minPos = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minPos]) {
+                    minPos = j;
+                }
+            }
+            int temp = array[minPos];
+            array[minPos] = array[i];
+            array[i] = temp;
+        }
+
+        return array;
+    }
+
+    public static <T extends Comparable<? super T>> List<T> selection(List<T> array) {
+        for (int i = 0; i < array.size() - 1; i++) {
+            int minPos = i;
+            for (int j = i + 1; j < array.size(); j++) {
+                if (array.get(j).compareTo(array.get(minPos)) < 0) {
+                    minPos = j;
+                }
+            }
+            swap(array, i, minPos);
+        }
+
+        return array;
+    }
+
 }
