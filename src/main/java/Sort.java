13776b1ce6ed7467364fcb47c941106113762c5b
yourass <yu.v.mur@gmail.com>
2022-03-01 18:01:45 +0300

    Add bubble sort with List

--- src/main/java/Sort.java
+++ src/main/java/Sort.java
@@ -5,21 +5,7 @@ import static java.util.Collections.swap;
 
 public class Sort {
 
-    public static <T extends Comparable<T>> List<T> bubble(List<T> array) {
-        for (int i = 0; i < array.size() - 1; i++) {
-            boolean swapped = false;
-            for (int j = 0; j < array.size() - 1 - i; j++) {
-                if (array.get(j).compareTo(array.get(j + 1)) > 0) {
-                    swap(array, j, j+1);
-                    swapped = true;
-                }
-            }
-            if (!swapped) break;
-        }
-        return array;
-    }
-
-    static int[] bubble (int[] array) {
+    public static int[] bubble (int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
             boolean swapped = false;
             for (int j = 0; j < array.length - 1 - i; j++) {
@@ -28,6 +14,20 @@ public class Sort {
                     array[j] = array[j+1];
                     array[j+1] = temp;
 
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+        return array;
+    }
+
+    public static <T extends Comparable<T>> List<T> bubble(List<T> array) {
+        for (int i = 0; i < array.size() - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < array.size() - 1 - i; j++) {
+                if (array.get(j).compareTo(array.get(j + 1)) > 0) {
+                    swap(array, j, j+1);
                     swapped = true;
                 }
             }
--- src/test/java/SortTest.java
+++ src/test/java/SortTest.java
@@ -13,15 +13,18 @@ public class SortTest {
         int[] array = {1, 7, 19, -2, 58, 43, 5};
         List<Integer> integerList = Arrays.asList(1, 7, 19, -2, 58, 43, 5);
         List<Double> doubleList = Arrays.asList(1.0, 7.0, 19.0d, -2.0, 58.44, 43.3, 5d);
+        List<String> stringList = Arrays.asList("D", "A", "C", "B");
 
         // when
         int[] sortedArray = Sort.bubble(array);
         List<Integer> sortedIntegerList = Sort.bubble(integerList);
         List<Double> sortedDoubleList = Sort.bubble(doubleList);
+        List<String> sortedStringList = Sort.bubble(stringList);
 
         // then
         assertArrayEquals(new int[]{-2, 1, 5, 7, 19, 43, 58}, sortedArray);
         assertArrayEquals(Arrays.asList(-2, 1, 5, 7, 19, 43, 58).toArray(), sortedIntegerList.toArray());
         assertArrayEquals(Arrays.asList(-2.0, 1.0, 5.0, 7.0, 19.0, 43.3, 58.44).toArray(), sortedDoubleList.toArray());
+        assertArrayEquals(Arrays.asList("A", "B", "C", "D").toArray(), sortedStringList.toArray());
     }
 }
