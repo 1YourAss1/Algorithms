922b3ce727ccd9bb4c9d5af7ce7d2769127c9794
yourass <yu.v.mur@gmail.com>
2022-03-01 22:48:42 +0300

    Add selection sort

--- src/main/java/Sort.java
+++ src/main/java/Sort.java
@@ -1,6 +1,4 @@
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static java.util.Collections.swap;
 
@@ -16,6 +14,22 @@ public class Sort {
                     array[j] = array[j+1];
                     array[j+1] = temp;
 
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+        return array;
+    }
+
+    public static <T extends Comparable<? super T>> T[] bubble(T[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j].compareTo(array[j + 1]) > 0) {
+                    T temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
                     swapped = true;
                 }
             }
@@ -55,6 +69,21 @@ public class Sort {
         return array;
     }
 
+    public static <T extends Comparable<? super T>> T[] selection(T[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int minPos = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j].compareTo(array[minPos]) < 0) {
+                    minPos = j;
+                }
+            }
+            T temp = array[minPos];
+            array[minPos] = array[i];
+            array[i] = temp;
+        }
+        return array;
+    }
+
     public static <T extends Comparable<? super T>> List<T> selection(List<T> array) {
         for (int i = 0; i < array.size() - 1; i++) {
             int minPos = i;
@@ -65,7 +94,6 @@ public class Sort {
             }
             swap(array, i, minPos);
         }
-
         return array;
     }
 
--- src/test/java/SortTest.java
+++ src/test/java/SortTest.java
@@ -1,28 +1,41 @@
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.Assert.assertArrayEquals;
 
 public class SortTest {
+    private static int[] array;
+    private static Integer[] integerArray;
+    private static List<Integer> integerList;
+    private static List<Double> doubleList;
+    private static List<String> stringList;
+
+    @BeforeClass
+    public static void setup() {
+        array = new int[] {1, 7, 19, -2, 58, 43, 5};
+        integerArray = new Integer[] {1, 7, 19, -2, 58, 43, 5};
+        integerList = Arrays.asList(1, 7, 19, -2, 58, 43, 5);
+        doubleList = Arrays.asList(1.0, 7.0, 19.0d, -2.0, 58.44, 43.3, 5d);
+        stringList = Arrays.asList("D", "A", "C", "B");
+    }
+
 
     @Test
     public void bubbleSortTest() {
-        // before
-        int[] array = {1, 7, 19, -2, 58, 43, 5};
-        List<Integer> integerList = Arrays.asList(1, 7, 19, -2, 58, 43, 5);
-        List<Double> doubleList = Arrays.asList(1.0, 7.0, 19.0d, -2.0, 58.44, 43.3, 5d);
-        List<String> stringList = Arrays.asList("D", "A", "C", "B");
-
         // when
         int[] sortedArray = Sort.bubble(array);
+        Integer[] sortedIntegerArray = Sort.bubble(integerArray);
         List<Integer> sortedIntegerList = Sort.bubble(integerList);
         List<Double> sortedDoubleList = Sort.bubble(doubleList);
         List<String> sortedStringList = Sort.bubble(stringList);
 
         // then
         assertArrayEquals(new int[]{-2, 1, 5, 7, 19, 43, 58}, sortedArray);
+        assertArrayEquals(new Integer[]{-2, 1, 5, 7, 19, 43, 58}, sortedIntegerArray);
         assertArrayEquals(Arrays.asList(-2, 1, 5, 7, 19, 43, 58).toArray(), sortedIntegerList.toArray());
         assertArrayEquals(Arrays.asList(-2.0, 1.0, 5.0, 7.0, 19.0, 43.3, 58.44).toArray(), sortedDoubleList.toArray());
         assertArrayEquals(Arrays.asList("A", "B", "C", "D").toArray(), sortedStringList.toArray());
@@ -30,20 +43,16 @@ public class SortTest {
 
     @Test
     public void selectionSortTest() {
-        // before
-        int[] array = {1, 7, 19, -2, 58, 43, 5};
-        List<Integer> integerList = Arrays.asList(1, 7, 19, -2, 58, 43, 5);
-        List<Double> doubleList = Arrays.asList(1.0, 7.0, 19.0d, -2.0, 58.44, 43.3, 5d);
-        List<String> stringList = Arrays.asList("D", "A", "C", "B");
-
         // when
         int[] sortedArray = Sort.selection(array);
+        Integer[] sortedIntegerArray = Sort.selection(integerArray);
         List<Integer> sortedIntegerList = Sort.selection(integerList);
         List<Double> sortedDoubleList = Sort.selection(doubleList);
         List<String> sortedStringList = Sort.selection(stringList);
 
         // then
         assertArrayEquals(new int[]{-2, 1, 5, 7, 19, 43, 58}, sortedArray);
+        assertArrayEquals(new Integer[]{-2, 1, 5, 7, 19, 43, 58}, sortedIntegerArray);
         assertArrayEquals(Arrays.asList(-2, 1, 5, 7, 19, 43, 58).toArray(), sortedIntegerList.toArray());
         assertArrayEquals(Arrays.asList(-2.0, 1.0, 5.0, 7.0, 19.0, 43.3, 58.44).toArray(), sortedDoubleList.toArray());
         assertArrayEquals(Arrays.asList("A", "B", "C", "D").toArray(), sortedStringList.toArray());
